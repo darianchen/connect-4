f47b05461642a45e8a9781aac6dea0b0357d13ca
Darian <darianychen@gmail.com>
2023-03-29 14:50:53 -0400

    Add css and logic for piece colors and add turn useState

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import './index.css';
 
 function Board() {
-  const BOARD = Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null));
+    const [turn, setTurn] = useState(1);
+    let board = Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null));
 
-  return (
-    <div id="board">
-        {BOARD.map((row, rowIdx) => {
-            return <div className="row" key={rowIdx}>
-                {row.map((col, colIdx) => {
-                    return <div className="cell" key={colIdx}>
-                        <div className='hole'></div>
+    return (
+        <>
+            <div id="board">
+                {board.map((row, rowIdx) => {
+                    return <div className="row" key={rowIdx}>
+                        {row.map((col, colIdx) => {
+                            return <div className="cell" key={colIdx}>
+                                <div className={`${board[rowIdx][colIdx] === null ? "empty" : board[rowIdx][colIdx] === "R" ? "red" : "yellow"}`}></div>
+                            </div>
+                        })}
                     </div>
                 })}
             </div>
-        })}
-    </div>
-  );
+        </>
+    );
 };
 
 export default Board;

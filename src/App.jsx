b14654ad215292cab4e103fc7e46da804a0c93f9
Darian <darianychen@gmail.com>
2023-03-29 21:36:23 -0400

    Implement drop piece function

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import Board from './Components/Board'
 
 function App() {
+  let board = Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null));
 
   return (
     <div className="App">
-      <Board />
+      <Board board={board} />
     </div>
   );
 };
--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react';
 import './index.css';
 
-function Board() {
+function Board({ board }) {
     const [turn, setTurn] = useState(1);
-    let board = Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => null));
+    const color = turn%2 === 0 ? 'yellow' : 'red';
+
+    const handleMove = (e) => {
+        const colIdx = e.target.getAttribute('idx');
+        e.target.classList.remove(color);
+        setTurn(turn + 1);
+        
+        for(let row = board.length - 1; row > -1; row--){
+            if(!board[row][colIdx]) {
+                board[row][colIdx] = color[0].toUpperCase();
+                console.log(board);
+                return;
+            }
+        }
+    }
+
+    const onMouseOver = (e) => {
+        e.target.classList.add(color);
+    }
+
+    const onMouseLeave = (e) => {
+        e.target.classList.remove(color);
+    }
 
     return (
         <div>
             <div id="dropzone">
                 {board[0].map((col, colIdx) => {
-                    return <div className="drop"></div>
+                    return <div key={colIdx} idx={colIdx} className="drop" onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} onClick={handleMove}></div>
                 })}
             </div>
             <div id="board">

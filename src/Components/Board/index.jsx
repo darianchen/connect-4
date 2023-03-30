f92709749b594940bb15373eb6f4c9939ec1cf94
Darian <darianychen@gmail.com>
2023-03-30 19:17:33 -0400

    Implement win condition

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -3,24 +3,61 @@
 
     function Board({ board }) {
         const [turn, setTurn] = useState(1);
+        const [gameOver, setGameOver] = useState(false);
         const color = turn%2 === 0 ? 'yellow' : 'red';
         const styles = { '--hover-color': color };
 
         const handleMove = (e) => {
-            const colIdx = e.target.getAttribute('idx');
+            const col = parseInt(e.target.getAttribute('idx'));
 
-            if(board[0][colIdx]) {
+            if(board[0][col]) {
                 alert("Column is full!");
                 return;
             };
 
             for(let row = board.length - 1; row > -1; row--){
-                if(!board[row][colIdx]) {
-                    board[row][colIdx] = color[0].toUpperCase();
+                if(!board[row][col]) {
+                    board[row][col] = color[0].toUpperCase();
+
+                    if(checkWin(row,col,color)) {
+                        setGameOver(true);
+                        alert("win");
+                        return;
+                    }
+
                     setTurn(turn + 1);
                     return;
                 }
             }
+        };
+
+        const checkWin = (row,col,color) => {
+            const dirs = [[1,0],[0,1],[1,1],[-1,1]];
+
+            for(let i = 0; i < dirs.length; i++){
+                let num = 1;
+                let[newRow,newCol] = [row+dirs[i][0], col+dirs[i][1]];
+                while(!outOfBounds(newRow,newCol) && board[newRow][newCol] && board[newRow][newCol] === color[0].toUpperCase()) { // keep checking
+                    num++;
+                    newRow += dirs[i][0];
+                    newCol += dirs[i][1];
+                };
+
+                //other direction
+                [newRow,newCol] = [row+ -dirs[i][0], col+ -dirs[i][1]];
+                while(!outOfBounds(newRow,newCol) && board[newRow][newCol] && board[newRow][newCol] === color[0].toUpperCase()) {
+                    num++;
+                    newRow += -dirs[i][0];
+                    newCol += -dirs[i][1];
+                };
+
+                if(num === 4) return true;
+            }
+            return false;
+        };
+
+        const outOfBounds = (newRow, newCol) => {
+            return newRow > 5 || newRow < 0 || newCol > 6 || newCol < 0
         };
 
         return (

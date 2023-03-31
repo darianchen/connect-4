d568bfbe36bda827b717fc71fe8fb0d5680df52f
Darian <darianychen@gmail.com>
2023-03-31 13:31:51 -0400

    Fix bug in win condition, format code, and remove borders in dropzone

--- src/Components/Board/index.jsx
+++ src/Components/Board/index.jsx
@@ -1,86 +1,121 @@
-    import { useState } from 'react';
-    import './index.css';
+import { useState } from "react";
+import "./index.css";
 
-    function Board({ board }) {
-        const [turn, setTurn] = useState(1);
-        const [gameOver, setGameOver] = useState(false);
-        const color = turn%2 === 0 ? 'yellow' : 'red';
-        const styles = { '--hover-color': color };
+function Board({ board }) {
+  const [turn, setTurn] = useState(1);
+  const [gameOver, setGameOver] = useState(false);
+  const color = turn % 2 === 0 ? "yellow" : "red";
+  const styles = { "--hover-color": color };
 
-        const handleMove = (e) => {
-            const col = parseInt(e.target.getAttribute('idx'));
+  const handleMove = (e) => {
+    const col = parseInt(e.target.getAttribute("idx"));
 
-            if(board[0][col]) {
-                alert("Column is full!");
-                return;
-            };
+    if (board[0][col]) {
+      alert("Column is full!");
+      return;
+    }
 
-            for(let row = board.length - 1; row > -1; row--){
-                if(!board[row][col]) {
-                    board[row][col] = color[0].toUpperCase();
+    for (let row = board.length - 1; row > -1; row--) {
+      if (!board[row][col]) {
+        board[row][col] = color[0].toUpperCase();
 
-                    if(checkWin(row,col,color)) {
-                        setGameOver(true);
-                        alert("win");
-                        return;
-                    }
+        if (checkWin(row, col, color, board)) {
+          setGameOver(true);
+          alert("win");
+          return;
+        }
 
-                    setTurn(turn + 1);
-                    return;
-                }
-            }
-        };
+        setTurn(turn + 1);
+        return;
+      }
+    }
+  };
 
-        const checkWin = (row,col,color) => {
-            const dirs = [[1,0],[0,1],[1,1],[-1,1]];
+  const checkWin = (row, col, color, board) => {
+    const dirs = [
+      [1, 0],
+      [0, 1],
+      [1, 1],
+      [-1, 1],
+    ];
 
-            for(let i = 0; i < dirs.length; i++){
-                let num = 1;
-                let[newRow,newCol] = [row+dirs[i][0], col+dirs[i][1]];
-                while(!outOfBounds(newRow,newCol) && board[newRow][newCol] && board[newRow][newCol] === color[0].toUpperCase()) { // keep checking
-                    num++;
-                    newRow += dirs[i][0];
-                    newCol += dirs[i][1];
-                };
+    for (let i = 0; i < dirs.length; i++) {
+      let num = 1;
+      let [newRow, newCol] = [row + dirs[i][0], col + dirs[i][1]];
+      while (
+        !outOfBounds(newRow, newCol) &&
+        board[newRow][newCol] &&
+        board[newRow][newCol] === color[0].toUpperCase()
+      ) {
+        // keep checking
+        num++;
+        newRow += dirs[i][0];
+        newCol += dirs[i][1];
+      }
 
-                //other direction
-                [newRow,newCol] = [row+ -dirs[i][0], col+ -dirs[i][1]];
-                while(!outOfBounds(newRow,newCol) && board[newRow][newCol] && board[newRow][newCol] === color[0].toUpperCase()) {
-                    num++;
-                    newRow += -dirs[i][0];
-                    newCol += -dirs[i][1];
-                };
+      //other direction
+      [newRow, newCol] = [row + -dirs[i][0], col + -dirs[i][1]];
+      while (
+        !outOfBounds(newRow, newCol) &&
+        board[newRow][newCol] &&
+        board[newRow][newCol] === color[0].toUpperCase()
+      ) {
+        num++;
+        newRow += -dirs[i][0];
+        newCol += -dirs[i][1];
+      }
 
-                if(num === 4) return true;
-            }
-            return false;
-        };
+      if (num >= 4) return true;
+    }
+    return false;
+  };
 
-        const outOfBounds = (newRow, newCol) => {
-            return newRow > 5 || newRow < 0 || newCol > 6 || newCol < 0
-        };
+  const outOfBounds = (newRow, newCol) => {
+    return newRow > 5 || newRow < 0 || newCol > 6 || newCol < 0;
+  };
 
-        return (
-            <div>
-                <div>Turn is {turn}</div>
-                <div id="dropzone" style={styles}>
-                    {board[0].map((col, colIdx) => {
-                        return <div key={colIdx} idx={colIdx} className="drop" onClick={handleMove}><div id="piece" idx={colIdx}></div></div>
-                    })}
+  return (
+    <div>
+      <div>Turn is {turn}</div>
+      <div id="dropzone" style={styles}>
+        {board[0].map((col, colIdx) => {
+          return (
+            <div
+              key={colIdx}
+              idx={colIdx}
+              className="drop"
+              onClick={handleMove}
+            >
+              <div id="piece" idx={colIdx}></div>
             </div>
-            <div id="board">
-                {board.map((row, rowIdx) => {
-                    return <div className="row" key={rowIdx}>
-                        {row.map((col, colIdx) => {
-                            return <div className="cell" key={colIdx}>
-                                <div className={`${board[rowIdx][colIdx] === null ? "empty" : board[rowIdx][colIdx] === "R" ? "red" : "yellow"}`}></div>
-                            </div>
-                        })}
-                    </div>
-                })}
+          );
+        })}
+      </div>
+      <div id="board">
+        {board.map((row, rowIdx) => {
+          return (
+            <div className="row" key={rowIdx}>
+              {row.map((col, colIdx) => {
+                return (
+                  <div className="cell" key={colIdx}>
+                    <div
+                      className={`${
+                        board[rowIdx][colIdx] === null
+                          ? "empty"
+                          : board[rowIdx][colIdx] === "R"
+                          ? "red"
+                          : "yellow"
+                      }`}
+                    ></div>
+                  </div>
+                );
+              })}
             </div>
-        </div>
-    );
-};
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Board;
